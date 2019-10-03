b7a9473bb8cd17ae012610bc5703933d4731eb42
Daniel Tuna <danieltuna97@gmail.com>
2019-10-03 17:49:22 +0300

    Added new trip

--- src/response/response.js
+++ src/response/response.js
@@ -1,93 +1,113 @@
-const response = [{
-    name: 'Weekend in Vegas',
-    image: 'vegas.jpg',
-    dateFrom: 'May 7th 2020',
-    dateTo: 'May 10th 2020',
-    guests: 2,
-    category: 'weekend break',
-    copy: 'Next level vexillologist taiyaki migas cloud bread post-ironic. Offal selvage gastropub bitters.'
-  },
-  {
-    name: 'Hiking in the Peaks',
-    image: 'peaks.jpg',
-    dateFrom: 'July 6th 2020',
-    dateTo: 'July 10th 2020',
-    guests: 2,
-    category: 'adventure',
-    copy: 'Helvetica umami selvage. Locavore gluten-free tacos, small batch banh mi shoreditch.'
-  },
-  {
-    name: 'Relaxing in Punta Cana',
-    image: 'puntacana.jpg',
-    dateFrom: 'Aug 7th 2020',
-    dateTo: 'Aug 9th 2020',
-    guests: 2,
-    category: 'weekend break',
-    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
-  },
-  {
-    name: 'Weekend at an Ice Hotel',
-    image: 'icehotel.jpg',
-    dateFrom: 'Jan 14th 2020',
-    dateTo: 'Jan 16th 2020',
-    guests: 4,
-    category: 'family',
-    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
-  },
-  {
-    name: 'Best of Rio de Janeiro',
-    image: 'rio.jpg',
-    dateFrom: 'Jan 14th 2020',
-    dateTo: 'Jan 21th 2020',
-    guests: 3,
-    category: 'weekend break',
-    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
-  },
-  {
-    name: 'Midsummer in Helsinki',
-    image: 'helsinki.jpg',
-    dateFrom: 'June 19th 2020',
-    dateTo: 'June 21th 2020',
-    guests: 2,
-    category: 'weekend break',
-    copy: 'Next level vexillologist taiyaki migas cloud bread post-ironic. Offal selvage gastropub bitters.'
-  },
-  {
-    name: 'Gardens of Damyang',
-    image: 'korea.jpg',
-    dateFrom: 'May 22nd 2020',
-    dateTo: 'May 30th 2020',
-    guests: 2,
-    category: 'adventure',
-    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
-  },
-  {
-    name: 'India',
-    image: 'india.jpg',
-    dateFrom: 'Dec 2nd 2019',
-    dateTo: 'Feb 9th 2020',
-    guests: 1,
-    category: 'family',
-    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
-  },
-  {
-    name: 'A Weekend in Vancouver',
-    image: 'vancouver.jpg',
-    dateFrom: 'Oct 5st 2019',
-    dateTo: 'Oct 8st 2019',
-    guests: 2,
-    category: 'family',
-    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
-  },
-  {
-    name: 'Decadent Week in Cape Town',
-    image: 'cape-town.jpg',
-    dateFrom: 'Oct 13th 2019',
-    dateTo: 'Oct 18th 2019',
-    guests: 4,
-    category: 'adventure',
-    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
-  }
+const response = [
+    {
+        name: 'Weekend in Vegas',
+        image: 'vegas.jpg',
+        dateFrom: 'May 7th 2020',
+        dateTo: 'May 10th 2020',
+        guests: 2,
+        category: 'weekend break',
+        copy:
+            'Next level vexillologist taiyaki migas cloud bread post-ironic. Offal selvage gastropub bitters.'
+    },
+    {
+        name: 'Hiking in the Peaks',
+        image: 'peaks.jpg',
+        dateFrom: 'July 6th 2020',
+        dateTo: 'July 10th 2020',
+        guests: 2,
+        category: 'adventure',
+        copy: 'Helvetica umami selvage. Locavore gluten-free tacos, small batch banh mi shoreditch.'
+    },
+    {
+        name: 'Relaxing in Punta Cana',
+        image: 'puntacana.jpg',
+        dateFrom: 'Aug 7th 2020',
+        dateTo: 'Aug 9th 2020',
+        guests: 2,
+        category: 'weekend break',
+        copy:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
+    },
+    {
+        name: 'Weekend at an Ice Hotel',
+        image: 'icehotel.jpg',
+        dateFrom: 'Jan 14th 2020',
+        dateTo: 'Jan 16th 2020',
+        guests: 4,
+        category: 'family',
+        copy:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
+    },
+    {
+        name: 'Best of Rio de Janeiro',
+        image: 'rio.jpg',
+        dateFrom: 'Jan 14th 2020',
+        dateTo: 'Jan 21th 2020',
+        guests: 3,
+        category: 'weekend break',
+        copy:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
+    },
+    {
+        name: 'Midsummer in Helsinki',
+        image: 'helsinki.jpg',
+        dateFrom: 'June 19th 2020',
+        dateTo: 'June 21th 2020',
+        guests: 2,
+        category: 'weekend break',
+        copy:
+            'Next level vexillologist taiyaki migas cloud bread post-ironic. Offal selvage gastropub bitters.'
+    },
+    {
+        name: 'Gardens of Damyang',
+        image: 'korea.jpg',
+        dateFrom: 'May 22nd 2020',
+        dateTo: 'May 30th 2020',
+        guests: 2,
+        category: 'adventure',
+        copy:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
+    },
+    {
+        name: 'India',
+        image: 'india.jpg',
+        dateFrom: 'Dec 2nd 2019',
+        dateTo: 'Feb 9th 2020',
+        guests: 1,
+        category: 'family',
+        copy:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
+    },
+    {
+        name: 'A Weekend in Vancouver',
+        image: 'vancouver.jpg',
+        dateFrom: 'Oct 5st 2019',
+        dateTo: 'Oct 8st 2019',
+        guests: 2,
+        category: 'family',
+        copy:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
+    },
+    {
+        name: 'Decadent Week in Cape Town',
+        image: 'cape-town.jpg',
+        dateFrom: 'Oct 13th 2019',
+        dateTo: 'Oct 18th 2019',
+        guests: 4,
+        category: 'adventure',
+        copy:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
+    },
+    {
+        name: 'Antwerpen',
+        image: 'antwerpen.jpg',
+        dateFrom: 'March 2nd 2020',
+        dateTo: 'March 4th 2020',
+        guests: 2,
+        category: 'weekend trip',
+        copy:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis.'
+    }
 ]
 
 export default response
