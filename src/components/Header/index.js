e976ae30262e50fd5ddb202507cf154c78784d33
S.Hale <sally.hale@homeport.org.uk>
2019-10-18 09:20:23 +0100

    Revert "Add burger menu to header"

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,7 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import {withRouter, Link} from 'react-router-dom'
 
 function Header({location, history}) {
-    let [burgerMenuVisible, showBurgerMenu] = useState(false)
-    let [dropdownActive, activateDropdown] = useState(false)
-
-    const closeDropdownIcon =
-            'https://upload.wikimedia.org/wikipedia/commons/7/72/VisualEditor_-_Icon_-_Close_-_white.svg',
-        burgerMenuIcon =
-            'https://upload.wikimedia.org/wikipedia/commons/5/59/Hamburger_icon_white.svg'
-
     useEffect(() => {
         window.addEventListener('scroll', () => {
             const scrollPosition = window.pageYOffset
@@ -22,24 +14,7 @@ function Header({location, history}) {
                 }
             }
         })
-
-        window.addEventListener('click', function closeDropdownIfNotClicked(e) {
-            let burgerMenu = document.getElementById('burger-menu')
-            e.target !== burgerMenu && activateDropdown(false)
-        })
-
-        window.innerWidth <= 560 && showBurgerMenu(true)
-
-        window.addEventListener('resize', function handleWindowResize() {
-            if (window.innerWidth <= 560) {
-                showBurgerMenu(true)
-            }
-            if (window.innerWidth > 560) {
-                showBurgerMenu(false)
-                activateDropdown(false)
-            }
-        })
-    }, [burgerMenuVisible, showBurgerMenu])
+    })
 
     function handleClickHome() {
         history.push('/')
@@ -61,68 +36,43 @@ function Header({location, history}) {
         history.push('/contact')
     }
 
-    function handleClickBurgerMenu() {
-        activateDropdown(!dropdownActive)
-    }
-
     return (
         <div className="header-container">
             <div className="header-wrap">
                 <nav>
                     <ul className="nav-bar">
-                        {burgerMenuVisible && (
-                            <li
-                                id="burger-menu-list-wrap"
-                                className={dropdownActive ? 'added-padding-bottom' : null}>
-                                <img
-                                    onClick={handleClickBurgerMenu}
-                                    id="burger-menu"
-                                    src={!dropdownActive ? burgerMenuIcon : closeDropdownIcon}
-                                    alt="Dropdown menu"
-                                />
-                            </li>
-                        )}
-                        <ul
-                            className={
-                                burgerMenuVisible
-                                    ? !dropdownActive
-                                        ? 'flex-dropdown inner-nav-bar hide'
-                                        : 'flex-dropdown inner-nav-bar'
-                                    : 'inner-nav-bar'
-                            }>
-                            <li className="opt-wrapper">
-                                <option className="nav-option" onClick={handleClickHome}>
-                                    home
-                                </option>
-                            </li>
-                            <li className="opt-wrapper">
-                                <option className="nav-option" onClick={handleClickAbout}>
-                                    about
-                                </option>
-                            </li>
-                            <li className="opt-wrapper">
-                                <option className="nav-option" onClick={handleClickExplore}>
-                                    explore
-                                </option>
-                            </li>
-                            <li className="opt-wrapper">
-                                <option className="nav-option" onClick={handleClickFaqs}>
-                                    faqs
-                                </option>
-                            </li>
-                            <li className="opt-wrapper">
-                                <option className="nav-option" onClick={handleClickContact}>
-                                    contact
-                                </option>
-                            </li>
-                            <li className="opt-wrapper">
-                                <a
-                                    className="nav-option"
-                                    href="https://github.com/s-Hale/BuildTravelCards">
-                                    GitHub
-                                </a>
-                            </li>
-                        </ul>
+                        <div className="opt-wrapper">
+                            <option className="nav-option" onClick={handleClickHome}>
+                                home
+                            </option>
+                        </div>
+                        <div className="opt-wrapper">
+                            <option className="nav-option" onClick={handleClickAbout}>
+                                about
+                            </option>
+                        </div>
+                        <div className="opt-wrapper">
+                            <option className="nav-option" onClick={handleClickExplore}>
+                                explore
+                            </option>
+                        </div>
+                        <div className="opt-wrapper">
+                            <option className="nav-option" onClick={handleClickFaqs}>
+                                faqs
+                            </option>
+                        </div>
+                        <div className="opt-wrapper">
+                            <option className="nav-option" onClick={handleClickContact}>
+                                contact
+                            </option>
+                        </div>
+                        <div className="opt-wrapper">
+                            <a
+                                className="nav-option"
+                                href="https://github.com/s-Hale/BuildTravelCards">
+                                GitHub
+                            </a>
+                        </div>
                     </ul>
                 </nav>
             </div>
